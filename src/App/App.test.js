ef463f084eb81df0af15784ae83ee7e39e417591
Jordan Quinn <jpquinn605@gmail.com>
2018-02-01 11:46:35 -0700

    fixed tests

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { shallow, mount } from 'enzyme';
 import renderer from 'react-test-renderer';
-import api from '../api'
-jest.mock(api)
+
+import { mockFilm } from '../mockData'
+
+import * as api from '../api'
 
 describe( 'App Component', () => {
   beforeAll(() => {
@@ -13,22 +15,19 @@ describe( 'App Component', () => {
         ok: true,
         status: 200,
         json: () => Promise.resolve({
-          scroll: {
-              "title": "A New Hope",
-              "episode_id": 4,
-              "opening_crawl": "It is a period of civil war.\r\nRebel spaceships, striking\r\nfrom a hidden base, have won\r\ntheir first victory against\r\nthe evil Galactic Empire.\r\n\r\nDuring the battle, Rebel\r\nspies managed to steal secret\r\nplans to the Empire's\r\nultimate weapon, the DEATH\r\nSTAR, an armored space\r\nstation with enough power\r\nto destroy an entire planet.\r\n\r\nPursued by the Empire's\r\nsinister agents, Princess\r\nLeia races home aboard her\r\nstarship, custodian of the\r\nstolen plans that can save her\r\npeople and restore\r\nfreedom to the galaxy....",
-              "release_date": "1977-05-25"
-          }
+          scroll: mockFilm 
         })
       })
     })
-   _api.getScroll = () => ({scroll: 'whytho'})
+   api.getScroll = () => (mockFilm)
   })
 
-  test('renders without crashing', async () => {
+  test('componentDidMount should set the proper state', async () => {
+    const expectedState = mockFilm
     const renderedComp = await shallow(<App />);
-    debugger;
+
     await renderedComp.update()
-    console.log(renderedComp.state())
+
+    expect(renderedComp.state('scroll')).toEqual(mockFilm)
   });
 });
